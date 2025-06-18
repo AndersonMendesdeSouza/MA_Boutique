363f37c1848b6608b9701167976510c85c42246f
AndersonMendesdeSouza <andersonmendesdesouza2007@gmail.com>
2025-06-17 22:33:45 -0300

    Categoria

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 
-const categoria = document.getElementById("categoria")
+// const categoria = document.getElementById("categoria")
 
-categoria.addEventListener("change", function(){
-    const Categoria = this.value;
+// categoria.addEventListener("change", function(){
+//     const Categoria = this.value;
     
-    const procurar = document.getElementById(Categoria)
+//     const procurar = document.getElementById(Categoria)
 
-if (procurar) {
-    procurar.scrollIntoView({ behavior: 'smooth' })};
-})
+// if (procurar) {
+//     procurar.scrollIntoView({ behavior: 'smooth' })};
+// })
 
  
+
+
+const Section = document.getElementById("categoria")
+Section.addEventListener("change", function(){
+    const valorSelect = this.value;
+   const lugarHtml = document.getElementById(valorSelect)
+
+   if(lugarHtml){
+    lugarHtml.scrollIntoView({behavior: 'smooth'} )
+   }
+})
